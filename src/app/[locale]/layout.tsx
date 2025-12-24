48d904fad6cfccaa624a9336a1313d3bd5f7f4af
Muhammad Mohsin <nuqtamenu@gmail.com>
2025-12-24 19:17:21 +0500

    Added Meta Pixel code

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -79,6 +79,24 @@ export default async function RootLayout({ children, params }: Props) {
             })(window,document,'script','dataLayer','GTM-WR5XFFMN');
           `}
         </Script>
+        {/* End Google Tag Manager */}
+        {/* Meta Pixel Code */}
+        <Script id="meta-pixel" strategy="beforeInteractive">
+          {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}
+      (window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '867005282643889');
+      fbq('track', 'PageView');
+    `}
+        </Script>
+        {/* End Meta Pixel Code */}
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
@@ -91,6 +109,20 @@ export default async function RootLayout({ children, params }: Props) {
           />
         </noscript>
         {/* End Google Tag Manager (noscript) */}
+
+        {/* Meta Pixel (noscript) */}
+        <noscript>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=867005282643889&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+        {/* End Meta Pixel (noscript) */}
+
         <NextIntlClientProvider>
           <HeaderContainer>
             <Header />
